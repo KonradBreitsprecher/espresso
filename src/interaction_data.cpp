04b10566064a4f98a29573998f9aacecb1a14916
Konrad Breitsprecher <konrad@yak.icp.uni-stuttgart.de>
2013-12-02 18:51:32 +0100

    subtr lj feature bug

--- src/interaction_data.cpp
+++ src/interaction_data.cpp
@@ -429,7 +429,7 @@ static void recalc_maximal_cutoff_bonded()
       if((bonded_ia_params[i].p.harmonic.r_cut>0)&&(max_cut_bonded < bonded_ia_params[i].p.harmonic.r_cut))
 	max_cut_bonded = bonded_ia_params[i].p.harmonic.r_cut;
       break;
-#if defined(ELECTROSTATICS)
+#ifdef(LENNARD_JONES)
     case BONDED_IA_SUBT_LJ:
       if(max_cut_bonded < bonded_ia_params[i].p.subt_lj.r)
 	max_cut_bonded = bonded_ia_params[i].p.subt_lj.r;
